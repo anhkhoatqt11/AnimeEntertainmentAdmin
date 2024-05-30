551fd317d8852814266c38f40b6120705cf101e6
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2024-05-30 11:16:42 +0700

    fix: Fix Donate API

--- src/app/api/donate/add/route.ts
+++ src/app/api/donate/add/route.ts
@@ -5,27 +5,14 @@ import mongoose from "mongoose";
 export async function POST(req: Request) {
     const body = await req.json();
 
-    const donates = await DonatePackage.updateMany(
-        {},
-        {
-            $push: {
-                donatepackagesCollection: {
-                    donatepackagesId: new mongoose.Types.ObjectId(),
-                    title: body.title,
-                    subtitle: body.subtitle,
-                    coin: body.coin,
-                    mediaUrl: body.mediaUrl,
-                    donateRecords: new mongoose.Types.Array(),
-                },
-            },
-        },
-        {
-            //options
-            returnNewDocument: true,
-            new: true,
-            strict: false,
-        }
-    );
+    const donates = DonatePackage.create({
+        title: body.title,
+        subtitle: body.subtitle,
+        coin: body.coin,
+        mediaUrl: body.mediaUrl,
+        donateRecords: new mongoose.Types.Array(),
+    })
+
     if (donates) {
         return new Response(JSON.stringify(donates), { status: 200 });
     }
