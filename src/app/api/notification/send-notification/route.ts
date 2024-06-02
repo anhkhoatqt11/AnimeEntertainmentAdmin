2ddf9c6816b072218d4c32cd859102f87a19849d
SonDangUIT1111 <sondanguit@gmail.com>
2024-06-02 15:54:14 +0700

    Link send notification to localhost 5000

--- src/app/api/notification/send-notification/route.ts
+++ src/app/api/notification/send-notification/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import UsersModel from "../../../../model/users";
 import mongoose from "mongoose";
+import { postRequest } from "@/lib/fetch";
 export async function POST(req: Request) {
   const body = await req.json();
 
@@ -24,6 +25,21 @@ export async function POST(req: Request) {
       strict: false,
     }
   );
+  const allUsers = await UsersModel.find();
+  allUsers.forEach(async (item) => {
+    const data = {
+      title: `Ping pong. ${
+        body.type === "chapter" ? "Chương mới đã ra mắt" : "Tập mới đã ra mắt"
+      }`,
+      body: body.content,
+      userId: item._id.toString(),
+    };
+    const res = await postRequest({
+      endPoint: "http://192.168.137.1:5000/api/users/sendPushNoti",
+      isFormData: false,
+      formData: data,
+    });
+  });
   if (users) {
     return new Response(JSON.stringify(users), { status: 200 });
   }
